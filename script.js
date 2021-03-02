987431cb05262f184f5ed3751a81a5cae7e0a483
shadialahmd <shadi.alahmd@gmail.com>
2021-03-02 14:51:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=99")
     .then(result => {
 
       return result.json();
@@ -15,8 +15,27 @@
       for(var i=0 ;i<r["results"].length;i++){
 
         var div=document.createElement("DIV");
+        var img=document.createElement("IMG");
+        var id=r["results"][i]["url"].slice(0,-1);
+        var l=id.slice(-2);
+     
+
+        console.log(l);
+        if(l%1== 0 ){
+
+            rr= id.slice(-2);
+            // console.log('number');
+         }
+         else {
+            rr= id.slice(-1);
+         }
+      
        div.innerHTML=r["results"][i]["name"];
+       img.src='https://pokeres.bastionbot.org/images/pokemon/'+rr+'.png';
+
+       document.body.appendChild(img);
        document.body.appendChild(div);
+
        
       
       }
