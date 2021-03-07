7045c8a4c60c0f682d7c8246f7c8e390a775563d
shadialahmd <shadi.alahmd@gmail.com>
2021-03-07 16:23:48 +0400

    css

--- s.js
+++ s.js
@@ -16,7 +16,7 @@ const colors = {
     ghost: '#db93de'
 }
 
-pokemon_count = 5;
+pokemon_count = 10;
 
 const fetchPokemons = async () => {
     console.log("start fetching");
@@ -31,13 +31,13 @@ const getPokemon = async (id) => {
     const res = await fetch(url)
     const data = await res.json()
     var div=document.createElement("DIV");
-    div.className="dd";
+    div.className="pok";
     var img=document.createElement("IMG");
    // div.innerHTML=id+' '+data['name'];
     div.innerHTML=`<div id="name">${data['name']}</div>`;
 
     div.style.backgroundColor=colors[data['types'][0]['type']['name']]
-    div.innerHTML+=`<img style="width:150px" src=https://pokeres.bastionbot.org/images/pokemon/${id}.png>`;
+    div.innerHTML+=`<div class="img-container"><img style="width:150px" src=https://pokeres.bastionbot.org/images/pokemon/${id}.png></div>`;
    
     // img.src='https://pokeres.bastionbot.org/images/pokemon/'+id+'.png';
     document.body.appendChild(div);
