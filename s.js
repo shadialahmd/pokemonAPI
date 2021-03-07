78a9f1753ecd98d50fa94bd6efceb439e0c890e9
shadialahmd <shadi.alahmd@gmail.com>
2021-03-07 12:45:52 +0400

    async await

---
+++ s.js
@@ -0,0 +1,32 @@
+
+
+
+async function api(){
+
+  
+    
+
+    let response = fetch('https://pokeapi.co/api/v2/pokemon/1')
+    let data =  await api2()
+    let data2 =  response.json()   
+    
+    console.log('api1');
+    console.log(data2)
+
+
+}
+
+
+function api2(){
+
+    for(x=0;x<5;x++){
+
+        console.log(x);
+    }
+   
+
+
+}
+//api2()
+
+api();
