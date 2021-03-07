2f6fff833578c66f718a9432c8c21223666c6bf3
shadialahmd <shadi.alahmd@gmail.com>
2021-03-07 14:48:14 +0400

    Update s.js

--- s.js
+++ s.js
@@ -1,29 +1,26 @@
+pokemon_count = 10;
 
+const fetchPokemons = async () => {
+    console.log("start fetching");
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
+    }
+    console.log("end fetching");
+}
 
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    var div=document.createElement("DIV");
+    var img=document.createElement("IMG");
+    div.innerHTML=id+' '+data['name'];
+    div.innerHTML+=`<img src=https://pokeres.bastionbot.org/images/pokemon/${id}.png>`;
   
+    // img.src='https://pokeres.bastionbot.org/images/pokemon/'+id+'.png';
+    document.body.appendChild(div);
+   // document.body.appendChild(img);
+    console.log(data);
     
-
-//     let response = fetch('https://pokeapi.co/api/v2/pokemon/1')
-//     let data =  await api2()
-//     //let data2 =  response.json()   
-    
-//     console.log('api1');
-//     //console.log(data2)
-
-
-// }
-
-
-// function api2(){
-
-//     for(x=0;x<5;x++){
-
-//         console.log(x);
-//     }
-   
-
-
-// }
-// //api2()
-
-// api().then(alert('ss'));
+}
+fetchPokemons();
